b0b12c63228ad65295a8812338ef536e92bde3cc
itsmeeandrew <schin.andras01@gmail.com>
2024-01-03 12:38:54 +0100

    Create preprocessed data

--- mlops/data/make_dataset.py
+++ mlops/data/make_dataset.py
@@ -1,3 +1,24 @@
+import torch
+from glob import glob
+
+def normalize_images(x):
+    means = x.mean(dim=(1, 2))
+    stds = x.std(dim=(1, 2))
+
+    return (x - means[:, None, None]) / stds[:, None, None]
+
+def load_and_save(prefix="train"):
+    image_files = glob(f"data/raw/{prefix}_images*.pt")
+    target_files = glob(f"data/raw/{prefix}_target*.pt")
+
+    images = torch.cat([torch.load(f) for f in image_files])
+    targets = torch.cat([torch.load(f) for f in target_files])
+
+    images = normalize_images(images)
+
+    torch.save(images, f"data/processed/{prefix}_images.pt")
+    torch.save(targets, f"data/processed/{prefix}_targets.pt")
+
 if __name__ == '__main__':
-    # Get the data and process it
-    pass
+    load_and_save("train")
+    load_and_save("test")
