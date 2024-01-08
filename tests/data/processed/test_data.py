15b602985e0b681bbb569282ecffccee44ac41c8
itsmeeandrew <schin.andras01@gmail.com>
2024-01-08 11:34:26 +0100

    Add tests

---
+++ tests/data/processed/test_data.py
@@ -0,0 +1,21 @@
+from mlops.train_model import load_dataset
+import pytest
+import os.path
+from tests import _PATH_DATA
+
+
+@pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
+def test_length():
+    trainloader, testloader = load_dataset(1)
+
+    assert len(trainloader) == 50000, "Length of trainloader is not as expected"
+    assert len(testloader) == 5000, "Length of testloader is not as expected"
+
+def test_shape():
+    trainloader, testloader = load_dataset(1)
+
+    for images, _ in trainloader:
+        assert images.shape == (1, 28, 28), "Shape of images is not as expected in trainloader"
+
+    for images, _ in testloader:
+        assert images.shape == (1, 28, 28), "Shape of images is not as expected in testloader"
